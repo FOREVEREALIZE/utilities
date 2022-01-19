76606d43b4714da5d6164769972b7db94632da60
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-01-19 12:31:47 +0100

    Create install_docker_compose_arm64.sh

---
+++ raspi/install_docker_compose_arm64.sh
@@ -0,0 +1,7 @@
+echo 'Installing Compose...'
+mkdir ~/.docker/cli-plugins
+sudo mkdir /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-armv64 -o /tmp/docker-compose
+cp /tmp/docker-compose ~/.docker/cli-plugins/docker-compose
+sudo cp /tmp/docker-compose /usr/local/lib/docker/cli-plugins
+echo 'Done!'
